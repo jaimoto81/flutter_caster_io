0d756c71acf0c13cb1088e4542033a8b66f8118b
Jaime Flautero <flaujaim@colpatria.com>
2020-08-29 14:21:39 -0500

    Addition of statefull widget and update state

--- lib/main.dart
+++ lib/main.dart
@@ -10,20 +10,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "You have pushed this buttone these many times",
-              ),
-              Text(
-                "0",
-                style: Theme.of(context).textTheme.display1,
-              )
-            ],
-          ),
+      home: MyHomePage(),
+      title: "Flutter Demo",
+      theme: ThemeData(
+        primaryColor: Colors.red,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyHomePageState();
+  }
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void incrementCounter(){
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        tooltip: "Increment",
+        onPressed: () {
+          incrementCounter();
+          print(_counter);
+        },
+      ),
+      appBar: AppBar(
+        title: Text("Flutter demo app"),
+        leading: Icon(Icons.burst_mode),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "You have pushed this buttone these many times",
+            ),
+            Text(
+              "$_counter ",
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
         ),
       ),
     );
